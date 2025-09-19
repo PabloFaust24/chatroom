058bf84bfe77b32d94a60de25fcc6c23946e5deb
PabloFaust24 <johnpaulgatdula24@gmail.com>
2025-09-19 16:12:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// Serve index.html (and other static files like CSS/JS)
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from "public" folder
+app.use(express.static('public'));
 
+// Socket.io
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  console.log('A user connected');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // send to everyone
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Listen
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
